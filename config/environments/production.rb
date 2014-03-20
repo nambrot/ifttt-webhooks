492274e23500be74793573365bec363eb94614a9
Nam Chu Hoai <nambrot@googlemail.com>
2014-03-20 18:44:09 -0400

    make this deploy already;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,12 +23,12 @@ IftttWebhooks::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :none
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.enabled = false
+  
   # Generate digests for assets URLs.
   config.assets.digest = true
 
